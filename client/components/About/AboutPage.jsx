c7438fe08b70de8f0b9c154daa58115b82b8af9b
Terrence Bowman <tbowman.oh@gmail.com>
2016-10-19 23:02:01 -0600

    Fleshing out more redux bouilerplate logic

--- client/components/About/AboutPage.jsx
+++ client/components/About/AboutPage.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <div>
       <h1>This is ohiock's Redux Starter</h1>
-      <p>I use this to start all of my react projects. Hopefully you will find it useful as well.</p>
+      <p>I built this to help me start my React projects faster. Maybe you'll find it useful, too!</p>
       <p>Test out this <Link to='/does-not-exist'>404 page</Link>.</p>
     </div>
   );
--- client/components/NotFoundPage.jsx
+++ client/components/NotFoundPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 const NotFoundPage = () => {
   return (
     <div>
       <h1>OOPS!</h1>
       <p>Looks like this page doesn't exist.</p>
-      <p>Head back <Link to='/home'>home</Link></p>
+      <p>Head back <IndexLink to='/'>home</IndexLink>.</p>
     </div>
   );
 };
