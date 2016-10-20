063a82d17154dfca97200fee74a9ecf5a534d6d5
Terrence Bowman <tbowman.oh@gmail.com>
2016-10-20 00:09:23 -0600

    Started saga logic

---
+++ client/sagas/index.js
@@ -0,0 +1,8 @@
+import { takeEvery, delay } from 'redux-saga';
+import { put } from 'redux-saga/effects';
+
+function* rootSaga() {
+  yield [];
+};
+
+export default rootSaga;
--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -2,10 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 export default function configureStore(initialState) {
-  const middlewares = [createSagaMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+
   const store = createStore(rootReducer, initialState, applyMiddleware(...middlewares);
+
+  sagaMiddleware.run(rootSaga);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
